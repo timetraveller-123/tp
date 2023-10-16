053bf53ebb903b53d4a5003fabe6f7b85c0aed27
joeng03 <yinjoeng2003@gmail.com>
2023-10-16 08:12:42 +0800

    test: Add invalid allergy test

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -39,7 +39,8 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
             @JsonProperty("email") String email, @JsonProperty("address") String address,
-            @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+            @JsonProperty("tags") List<JsonAdaptedTag> tags,
+            @JsonProperty("allergies") List<JsonAdaptedAllergy> allergies) {
         this.name = name;
         this.phone = phone;
         this.email = email;
