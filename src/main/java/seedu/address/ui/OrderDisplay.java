2349d4a54b7bd81cb9706db2ec1b26b597753c58
Lin Wan Lei <111512031+LinWanLeii@users.noreply.github.com>
2023-10-17 13:40:24 +0800

    Add order example to simulate

--- src/main/java/seedu/address/ui/OrderDisplay.java
+++ src/main/java/seedu/address/ui/OrderDisplay.java
@@ -6,6 +6,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
+import seedu.address.model.order.Order;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+
 
 /**
  * A ui for the status bar that is displayed at the header of the application.
@@ -14,13 +21,24 @@ public class OrderDisplay extends UiPart<Region> {
 
     private static final String FXML = "OrderDisplay.fxml";
 
+    private final Person testJane = new Person(new Name("Jane"), new Phone("98765432"),
+            new Email("jane@mail.com"), new Address("woodlands"), null );
+    private final Order order = new Order(2, testJane, "panadol");
     @FXML
     private TextArea resultDisplay;
     @FXML
-    private Label orderDetail;
+    private Label orderNumber;
+    @FXML
+    private Label orderPerson;
+    @FXML
+    private Label orderDetails;
 
+    /**
+     * Creates the order display.
+     */
     public OrderDisplay() {
         super(FXML);
+        orderNumber.setText(order.toString());
     }
 
     public void setFeedbackToUser(String feedbackToUser) {
