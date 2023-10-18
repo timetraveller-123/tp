89e0ee0d292076c5852a0a3ead20dd2926510329
HEEaZ <do.u.noe.imm@gmail.com>
2023-10-18 12:40:29 +0800

    Update CommandResult with InfoObject attribute

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -5,6 +5,7 @@ import static java.util.Objects.requireNonNull;
 import java.util.Objects;
 
 import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.InfoObject;
 
 /**
  * Represents the result of a command execution.
@@ -19,13 +20,17 @@ public class CommandResult {
     /** The application should exit. */
     private final boolean exit;
 
+    /** Info object to be displayed */
+    private final InfoObject infoObject;
+
     /**
      * Constructs a {@code CommandResult} with the specified fields.
      */
-    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit) {
+    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit, InfoObject infoObject) {
         this.feedbackToUser = requireNonNull(feedbackToUser);
         this.showHelp = showHelp;
         this.exit = exit;
+        this.infoObject = infoObject;
     }
 
     /**
@@ -33,11 +38,19 @@ public class CommandResult {
      * and other fields set to their default value.
      */
     public CommandResult(String feedbackToUser) {
-        this(feedbackToUser, false, false);
+        this(feedbackToUser, false, false, null);
+    }
+
+    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit) {
+        this(feedbackToUser, showHelp, exit, null);
     }
 
     public String getFeedbackToUser() {
         return feedbackToUser;
+    }
+
+    public InfoObject getInfoObject() {
+        return infoObject;
     }
 
     public boolean isShowHelp() {
@@ -46,6 +59,10 @@ public class CommandResult {
 
     public boolean isExit() {
         return exit;
+    }
+
+    public boolean hasInfoObject() {
+        return infoObject != null;
     }
 
     @Override
