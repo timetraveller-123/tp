5ed955d035cdf57ef6bd8ed5739f39f33c42d275
HEEaZ <do.u.noe.imm@gmail.com>
2023-10-18 14:46:19 +0800

    Add tests for ViewOrderCommandParser

---
+++ src/test/java/seedu/address/logic/parser/ViewOrderCommandParserTest.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.parser;
+
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.ViewOrderCommand;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+class ViewOrderCommandParserTest {
+    private ViewOrderCommandParser parser = new ViewOrderCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsViewOrderCommand() {
+        assertParseSuccess(parser, "1", new ViewOrderCommand(1));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewOrderCommand.MESSAGE_USAGE));
+    }
+
+}
