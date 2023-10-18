b10fce274956059f542f82709ccf0e73a55fea4a
HEEaZ <do.u.noe.imm@gmail.com>
2023-10-18 19:13:55 +0800

    Add tests for ListOrderCommand

---
+++ src/test/java/seedu/address/logic/commands/ListOrderCommandTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListOrderCommand.
+ */
+class ListOrderCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        CommandResult expectedCommandResult = new CommandResult(
+                ListOrderCommand.MESSAGE_SUCCESS, CommandResult.ListPanelEffects.ORDER);
+        assertCommandSuccess(new ListOrderCommand(), model, expectedCommandResult, expectedModel);
+    }
+
+}
