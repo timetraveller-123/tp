0b5b0566d80decaa3a21a96ff01a9069588026ce
joeng03 <yinjoeng2003@gmail.com>
2023-10-24 06:44:17 +0800

    test: Add test case for allergic and not ignore allergy

--- src/test/java/seedu/address/logic/commands/AddOrderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddOrderCommandTest.java
@@ -60,7 +60,8 @@ class AddOrderCommandTest {
         assertCommandSuccess(addOrderCommand, model, expectedMessage, expectedModel);
     }
 
-    @Test void execute_allergicToMedicineAndIgnoreAllergy_success() {
+    @Test
+    void execute_allergicToMedicineAndIgnoreAllergy_success() {
         Person person = new PersonBuilder().withAllergies(medicineName).build();
         model.addPerson(person);
         AddOrderCommand addOrderCommand = new AddOrderCommand(INDEX_FIRST_PERSON, orderNumber, medicineName, true);
@@ -90,6 +91,21 @@ class AddOrderCommandTest {
         expectedModel.addOrder(order);
 
         assertCommandFailure(addOrderCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    void execute_allergicToMedicineAndNotIgnoreAllergy_failure() {
+        Person person = new PersonBuilder().withAllergies(medicineName).build();
+        model.addPerson(person);
+        AddOrderCommand addOrderCommand = new AddOrderCommand(Index.fromOneBased(model.getFilteredPersonList().size()),
+                orderNumber, medicineName,
+                false);
+
+        Order order = new Order(orderNumber, model.getFilteredPersonList().get(0), medicineName);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.addOrder(order);
+
+        assertCommandFailure(addOrderCommand, model, Messages.MESSAGE_ALLERGIC_TO_MEDICINE);
     }
 
     @Test
