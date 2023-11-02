e70ad31f6bd91d08a7f06e126ab1dd6ace30c235
HEEaZ <do.u.noe.imm@gmail.com>
2023-11-02 16:09:00 +0800

    Create Ui components

--- src/main/java/seedu/address/ui/PersonDisplay.java
+++ src/main/java/seedu/address/ui/PersonDisplay.java
@@ -3,13 +3,20 @@ package seedu.address.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.StringUtil;
+import seedu.address.model.order.Status;
 import seedu.address.model.person.Person;
 
+import java.util.EnumMap;
+import java.util.logging.Logger;
+
 /**
- * A ui for the status bar that is displayed at the header of the application.
+ * An ui for the status bar that is displayed at the header of the application.
  */
 public class PersonDisplay extends UiPart<Region> {
+    private final Logger logger = LogsCenter.getLogger(OrderListPanel.class);
 
     private static final String FXML = "PersonDisplay.fxml";
 
@@ -23,6 +30,14 @@ public class PersonDisplay extends UiPart<Region> {
     private Label email;
     @FXML
     private Label allergies;
+    @FXML
+    private VBox pendingOrderVbox;
+    @FXML
+    private VBox preparingOrderVbox;
+    @FXML
+    private VBox completedOrderVbox;
+    @FXML
+    private VBox cancelledOrderVbox;
 
     /**
      * Creates the person display.
@@ -35,5 +50,32 @@ public class PersonDisplay extends UiPart<Region> {
         phoneNumber.setText(person.getPhone().value);
         email.setText(person.getEmail().value);
         allergies.setText(StringUtil.unmodifiableSetToCommaSeparatedStr(person.getAllergies()));
+
+        EnumMap<Status.OrderStatus, Integer> indexes = new EnumMap<>(Status.OrderStatus.class);
+        for (Status.OrderStatus e : Status.OrderStatus.values()) {
+            indexes.put(e, 1);
+        }
+
+        person.getOrders().forEach(o -> {
+            switch (o.getStatus().getStatus()) {
+                case PENDING:
+                    pendingOrderVbox.getChildren().add(
+                            new OrderCard(o, indexes.get(Status.OrderStatus.PENDING)).getRoot());
+                    indexes.put(Status.OrderStatus.PENDING, indexes.get(Status.OrderStatus.PENDING) + 1);
+                    break;
+                case PREPARING:
+                    preparingOrderVbox.getChildren().add(
+                            new OrderCard(o, indexes.get(Status.OrderStatus.PREPARING)).getRoot());
+                    indexes.put(Status.OrderStatus.PREPARING, indexes.get(Status.OrderStatus.PREPARING) + 1);
+                    break;
+                case COMPLETED:
+                    completedOrderVbox.getChildren().add(
+                            new OrderCard(o, indexes.get(Status.OrderStatus.COMPLETED)).getRoot());
+                    indexes.put(Status.OrderStatus.COMPLETED, indexes.get(Status.OrderStatus.COMPLETED) + 1);
+                    break;
+                default:
+                    throw new RuntimeException("Unknown order status type");
+            }
+        });
     }
 }
