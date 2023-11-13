983c10c978a96b0b1d75c3c36abf2afe6c969215
joeng03 <yinjoeng2003@gmail.com>
2023-11-13 21:47:44 +0800

    fix: Incorrect error text

--- src/main/java/seedu/pharmhub/logic/commands/AddPersonCommand.java
+++ src/main/java/seedu/pharmhub/logic/commands/AddPersonCommand.java
@@ -2,6 +2,7 @@ package seedu.pharmhub.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_ALLERGY;
 import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -34,7 +35,8 @@ public class AddPersonCommand extends Command {
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "[" + PREFIX_TAG + "TAG]... "
+            + "[" + PREFIX_ALLERGY + "ALLERGY]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
--- src/main/java/seedu/pharmhub/logic/commands/EditPersonCommand.java
+++ src/main/java/seedu/pharmhub/logic/commands/EditPersonCommand.java
@@ -2,6 +2,7 @@ package seedu.pharmhub.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_ALLERGY;
 import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.pharmhub.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -47,7 +48,8 @@ public class EditPersonCommand extends Command {
             + "[" + PREFIX_PHONE + "PHONE] "
             + "[" + PREFIX_EMAIL + "EMAIL] "
             + "[" + PREFIX_ADDRESS + "ADDRESS] "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + "[" + PREFIX_TAG + "TAG]... "
+            + "[" + PREFIX_ALLERGY + "ALLERGY]...\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_PHONE + "91234567 "
             + PREFIX_EMAIL + "johndoe@example.com";
